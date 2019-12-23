202e7ded17c71fbcf2db4079baa023f8b1d180c3
syedmeesamali <syedmeesamali@yahoo.com>
2019-12-24 00:56:42 +0400

    Buzzer combined with LED blinking

--- 1_Ultrasonic_Sensor/Sonar01/Sonar01.ino
+++ 1_Ultrasonic_Sensor/Sonar01/Sonar01.ino
@@ -1,14 +1,29 @@
-#include <NewPing.h>
-
-int trigPin = 11;
-int echoPin = 12;
+int trigPin = 11;       //Trigger pin
+int echoPin = 12;       //Echo response pin
 long duration, cm, inches;
+const int buzzer = 8;
 
+void playBuzzer() {
+  pinMode (buzzer, OUTPUT);
+  delay (10);
+  analogWrite (buzzer, 0);
+  delay (10);
+  analogWrite (buzzer, 255);
+  delay (10);
+}
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+}
+
+void LED_Blink() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
 }
 
 void loop() {
@@ -21,6 +36,12 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   cm = (duration/2) / 29.1;
   inches = (duration/2) / 74;
+  if (inches < 3)
+  {
+    playBuzzer();
+    LED_Blink();
+  }
+  
   Serial.print(inches);
   Serial.print("in, ");
   Serial.print(cm);
