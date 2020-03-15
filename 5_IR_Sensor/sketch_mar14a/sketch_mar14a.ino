07a69c2d4e82ee764f2844cbb20d9fe1f4ab637a
syedmeesamali <syedmeesamali@yahoo.com>
2020-03-16 01:20:02 +0400

    Laser ON/OFF control with remote control

--- 5_IR_Sensor/sketch_mar14a/sketch_mar14a.ino
+++ 5_IR_Sensor/sketch_mar14a/sketch_mar14a.ino
@@ -1,30 +1,38 @@
 #include<IRremote.h>
 
-int IRSensor = 2;
+const int IR_Recv = 2;
+const int laser = 8;
 int LED = 13;
 
-IRrecv irrecv(IRSensor); //IR receive object
+IRrecv irrecv(IR_Recv); //IR receive object
 decode_results decodedSignal; //Store results from detector
 
 void setup () {
-  pinMode (IRSensor, OUTPUT);
-  //Serial.begin(9600);
+  pinMode (laser, OUTPUT);
+  Serial.begin(9600);
   irrecv.enableIRIn();
 }
 
-boolean lightState = false;
-unsigned long last = millis();
+//boolean lightState = false;
+//unsigned long last = millis();
 
 void loop () {
-  if (irrecv.decode(&decodedSignal) == true)
-  
+  if (irrecv.decode(&decodedSignal))
   {
     Serial.println(decodedSignal.value, HEX);
-    if (millis() - last > 250){
-      lightState = !lightState;
-      digitalWrite(LED, lightState);
+    switch(decodedSignal.value)
+    {
+      case 0xFD20DF:
+          Serial.println("Button 4 pressed! Laser ON");
+          digitalWrite(laser, LOW); //Makes it ON
     }
-    last = millis();
+
+    switch(decodedSignal.value)
+    {
+    case 0xFDA05F:
+          Serial.println("Button 5 pressed ! Laser OFF");
+          digitalWrite(laser, HIGH); //High makes it OFF
+    }
     irrecv.resume();
   }
     
