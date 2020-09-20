07bda6c32c27c51dc4459578ee3464f555deb292
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-20 22:00:47 +0400

    Try to build simple windows app for controlling / directing arduino

---
+++ 0_CSharp_Win_Arudino_Control/Arduino_Serial/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Arduino_Serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Start Receiving:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pBar
+            // 
+            this.pBar.Location = new System.Drawing.Point(67, 169);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(481, 43);
+            this.pBar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 368);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arduino Serial Connection";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar pBar;
+    }
+}
+
---
+++ 0_CSharp_Win_Arudino_Control/Arduino_Serial/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arduino_Serial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Main click button to receive and dispaly data
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pBar.Value += 5;
+            if (pBar.Value == 100)
+            {
+                pBar.Value = 0;
+            }
+        }
+    }
+}
