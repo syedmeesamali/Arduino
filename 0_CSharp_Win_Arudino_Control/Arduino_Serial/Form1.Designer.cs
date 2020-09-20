bd7001cb38d5047f2640bb1bb954c9eefa5798d8
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-20 23:23:49 +0400

    Some primary port related stuff working but unable to read useful data

--- 0_CSharp_Win_Arudino_Control/Arduino_Serial/Form1.Designer.cs
+++ 0_CSharp_Win_Arudino_Control/Arduino_Serial/Form1.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pBar = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -50,11 +51,21 @@
             this.pBar.Size = new System.Drawing.Size(481, 43);
             this.pBar.TabIndex = 1;
             // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(253, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 26);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(734, 368);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pBar);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -63,6 +74,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Arduino Serial Connection";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +82,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ProgressBar pBar;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- 0_CSharp_Win_Arudino_Control/Arduino_Serial/Form1.cs
+++ 0_CSharp_Win_Arudino_Control/Arduino_Serial/Form1.cs
@@ -1,30 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Arduino_Serial
 {
     public partial class Form1 : Form
     {
+        SerialPort serialPort;      //Define the main serial port object
+        static bool _continue;
+        
+        //Main form initialization only
         public Form1()
         {
             InitializeComponent();
+            if (serialPort == null)
+            {
+                serialPort = new SerialPort("COM3", 9600);      //Common baud rate of 9600
+                serialPort.Open();
+                _continue = true;
+            }
+
         }
+
 
         //Main click button to receive and dispaly data
         private void button1_Click(object sender, EventArgs e)
         {
-            pBar.Value += 5;
-            if (pBar.Value == 100)
+            
+            if (serialPort != null)
             {
-                pBar.Value = 0;
+                while (_continue)
+                {
+                    int readValue = 0;
+                    int calibration = 0;
+                    calibration = ReadValues(readValue);
+                    textBox1.Text = calibration.ToString();
+                    if (calibration > 100)
+                    {
+                        int diff = calibration - 100;
+                        pBar.Value = calibration - diff;
+                    } else
+                    {
+                        pBar.Value = calibration;
+                    }
+                    
+                }
+            } else
+            {
+                serialPort = new SerialPort("COM3", 9600);      //Common baud rate of 9600
+                serialPort.Open();
+                _continue = true;
             }
         }
-    }
-}
+
+        //Simple function to reach character from serial port
+        private int ReadValues(int msg)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                msg = serialPort.ReadChar();
+            }
+            return msg;
+        }
+
+    }//End of main class
+}//End of namespace
