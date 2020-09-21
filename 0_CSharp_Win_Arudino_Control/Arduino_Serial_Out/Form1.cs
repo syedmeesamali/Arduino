4b26202baee1012b87e9f571f953349a3505fb3f
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-21 22:10:34 +0400

    Excellent work on the serial - out - Control of arduino by output from Windows C# form

---
+++ 0_CSharp_Win_Arudino_Control/Arduino_Serial_Out/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Arduino_Serial
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;      //Define the main serial port object
+        //Main form initialization only
+        public Form1()
+        {
+            InitializeComponent();
+            if (serialPort == null)
+            {
+                serialPort = new SerialPort("COM3", 9600);      //Common baud rate of 9600
+                serialPort.Open();
+            }
+        }
+
+        //Main click button to receive and dispaly data
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string valToSend;
+            valToSend = textBox1.Text;
+            serialPort.Write(valToSend);
+        }
+    }//End of main class
+}//End of namespace
--- 8_Serial/8_Serial.ino
+++ 8_Serial/8_Serial.ino
@@ -1,14 +1,21 @@
-int photo = 0;   //Analog input pin
-int lightVal;
+const int led = 8;
+int ledState = 0;
 
 void setup() {
-  pinMode(photo, INPUT);
+  pinMode(led, OUTPUT);
   Serial.begin(9600);  // Debugging only
 }
 
 //Main loop of arduino
 void loop() {
-    lightVal = analogRead(photo);
-    Serial.println(lightVal);
-    delay(500);
+    char receiveVal;
+    if(Serial.available() > 0)
+    {
+      receiveVal = Serial.read();     //Value to be read from the serial port - if 1 turn ON else anything turn OFF
+      if (receiveVal == '1')
+        ledState = 1;
+       else 
+        ledState = 0;
+    }
+    digitalWrite(led, ledState);      //Update the LED value
 }
