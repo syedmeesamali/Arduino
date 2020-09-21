8ad07bcf33eac36a1185f86e112a63c57e215448
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-21 23:46:53 +0400

    Bluetooth connection to arduino with CSharp - Awesome accompalishmens today - this one not completed though

---
+++ 0_CSharp_Win_Arudino_Control/Arduino_Serial_Bluetooth/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Arduino_Serial
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;      //Define the main serial port object
+        //Main form initialization only
+        public Form1()
+        {
+            InitializeComponent();
+            if (serialPort == null)
+            {
+                serialPort = new SerialPort("COM5", 9600);      //Common baud rate of 9600
+                serialPort.Open();
+            }
+        }
+
+        //Main click button to receive and dispaly data
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string valToSend;
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "0";
+            } else
+            {
+                valToSend = textBox1.Text;
+                serialPort.Write(valToSend);
+            }
+            
+        }
+    }//End of main class
+}//End of namespace
