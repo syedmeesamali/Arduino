637211040434ee682b579506f0b8604078e65299
syedmeesamali <syedmeesamali@yahoo.com>
2020-09-26 06:59:42 +0400

    Great effort to connect HC-05 through web-app but unsuccessful

--- 5_Bluetooth-App/app.js
+++ 5_Bluetooth-App/app.js
@@ -1,31 +1,59 @@
-navigator.getUserMedia = 
-    navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+const listBT = document.getElementById("list-BT");
+const deg_0 = document.getElementById("0_deg");
+const deg_90 = document.getElementById("90_deg");
 
-const video = document.querySelector('#video');
-const canvas = document.querySelector('#canvas');
-const context = canvas.getContext('2d');
-const audio = document.querySelector('#audio');
-let model;
+listBT.addEventListener('click', function (event) {
+    event.preventDefault();    
+    console.log("List BT clicked");
+    navigator.bluetooth.requestDevice({
+        filters: [{
+            name: 'HC-05'
+          }], optionalServices: ['battery_service']
+      })
+      .then(device => { /* ... */ })
+      .catch(error => { console.log(error); });
+    
+});
 
-handTrack.startVideo(video).then(status => {
-    if (status) {
-        navigator.getUserMedia({ video: {}}, stream => { 
-            video.srcObject = stream;
-            setInterval(runDetection, 1000);
-        }, err => console.error(err)) 
-    }
+deg_0.addEventListener('click', function (event) {
+    event.preventDefault(); 
+    console.log("0 degrees clicked");
+    navigator.bluetooth.requestDevice({
+        acceptAllDevices: true
+    })
+    .then(device => {
+        console.log('> Name:             ' + device.name);
+        console.log('> Id:               ' + device.id);
+        console.log('> Connected:        ' + device.gatt.connected);
+  })
+  .catch(error => {
+    console.log('Argh! ' + error);
+  });
 })
 
-function runDetection() {
-    model.detect(video).then(predictions => {
-        //model.renderPredictions(predictions, canvas, context, video); //Will really slow down
-        if(predictions.length > 0) {
-            audio.play();
-        }
-    });
-}
+deg_90.addEventListener('click', function (event) {
+    event.preventDefault();    
+    console.log("90 degrees clicked");
+})
 
+/*
+function connectBT() {
+    chrome.permission.request({
+        permissions: ['tabs'], origins: ['http://www.google.com/']}, function(granted) {
+            if (granted) {
+                navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+                .then(device => {
+                    // Human-readable name of the device.
+                    console.log(device.name);
 
-handTrack.load().then(lmodel => {
-    model = lmodel;
-});
+                    // Attempts to connect to remote GATT Server.
+                    return device.gatt.connect();
+                })
+                .then(server => { })
+                .catch(error => { console.log(error); });
+            } else {
+                alert("Permission not granted: Sorry !!!")
+            }
+          
+        });
+} */
