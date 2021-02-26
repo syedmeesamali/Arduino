bf9ec4fccb954b107184e420b1864346e292105b
syedmeesamali <syedmeesamali@yahoo.com>
2021-02-26 23:46:09 +0400

    General review of arm using Arduino

--- 10_Dome_Arm/Dome_Arm_01/Dome_Arm_01.ino
+++ 10_Dome_Arm/Dome_Arm_01/Dome_Arm_01.ino
@@ -1,24 +1,30 @@
 #include<Servo.h>
 Servo base;           //Servo motor for the base
+Servo lower_arm;           //Servo motor for the base
+Servo main_arm;           //Servo motor for the base
+Servo gripper;           //Servo motor for the base
 
 //Main program setup
 void setup() {
   Serial.begin(9600);
-  base.attach(8);     //Attach on pin # 8 of arduino
+  gripper.attach(8);     //Attach on pin # 8 of arduino
+  base.attach(10);     //Attach on pin # 8 of arduino
+  lower_arm.attach(7);     //Attach on pin # 8 of arduino
+  main_arm.attach(9);     //Attach on pin # 8 of arduino
 }
 
 //Main running loop of program
 void loop() {
-  for (int i=0; i <= 180; i = i + 5)
+  for (int i=60; i <= 120; i = i + 5)
   {
-    base.write(i);
+    gripper.write(i);
+    delay(500);
+  }
+  delay(1500);
+  for (int j = 120; j >= 60; j = j - 5)
+  {
+    gripper.write(j);
     delay(200);
   }
   delay(1500);
-  for (int j = 180; j > 0; j = j - 5)
-  {
-    base.write(j);
-    delay(200);
-  }
-  delay(2000);
 }
